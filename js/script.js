3ee909fdbd3e63550e07e01f05c1f292d7dfee46
AhmadHirthani <hertani86@gmail.com>
2020-06-13 21:43:43 +0300

    add style using css

--- js/script.js
+++ js/script.js
@@ -6,14 +6,8 @@ var myAge = 33;
 var myGender = 'male';
 var myFavColor = 'red';
 var myEducationDegree = 'master';
-//var myRandomNumber = 150;
 var myRandomNumber = Math.floor(Math.random() * (200 - 100)) + 100;
 var myBelovedPlaces = ['vienna', 'gaza', 'cairo', 'amman', 'usa', 'uk'];
-
-console.log('myRandomNumber', myRandomNumber);
-
-
-
 
 var name;
 var age;
@@ -23,7 +17,7 @@ var educationDegree;
 var randomNumber;
 var belovedPlaces;
 
-
+var nameValid = false;
 var ageValid = false;
 var genderValid = false;
 var favColorValid = false;
@@ -43,123 +37,149 @@ var educationDegreeAnswer = false;
 var randomNumberAnswer = false;
 var belovedPlacesAnswer = false;
 
-function Fivequesation(que,rightanswer,wronganswer)
- {
-    var nameValid = false;
-    while (!nameValid) {
-        name = prompt(que,'Yes , y , No, n ').toLocaleLowerCase();
-        if (name == 'yes' || name == 'y' || name == 'no' || name == 'n') {
-            nameValid = true;
-        }
+
+while (!nameValid) {
+    name = prompt('My name is Ahmad Alhrthani', 'Yes, Y, No or N').toLocaleLowerCase();
+    if (name == 'yes' || name == 'y' || name == 'no' || name == 'n') {
+        nameValid = true;
     }
-
-    if (name == 'yes' || name == 'y') {
-        correctAnswersCount++;
-        alert(rightanswer);
-        nameAnswer = true;
-    } else {
-        alert(wronganswer);
-    }
-
-
-
-
 }
 
-Fivequesation('My name is Ahmad Alhrthani', 'Yes, My Name Ahmad Alhrthani','Sorry, You should remeber my name, I am Ahmad Alhrthani');
-Fivequesation('My Age is 22', 'Yes, Y, No or N', 'No , My Age 33','Yes, My age 33');
-Fivequesation('My favourite color is Blue', 'No, My favourite color is Red','Yes,Correct My favourite color is Red');
-Fivequesation('My Gender is Male', 'Yes, Y, No or N', 'Yes, Male','No, My Gender is Male');
-Fivequesation('My education grade is Master', 'Yes,I have Master Dgree','No,that\'s wrong I have Master dgree');
+if (name == 'yes' || name == 'y') {
+    correctAnswersCount++;
+    nameAnswer = true;
+} else {
+    alert('Sory, You should remeber my name, I am Ahmad Alhrthani');
+}
 
+while (!ageValid) {
+    age = prompt('My Age is 22', 'Yes, Y, No or N').toLocaleLowerCase();
+    if (age == 'yes' || age == 'y' || age == 'no' || age == 'n') {
+        ageValid = true;
+    }
+}
+
+if (age == 'no' || age == 'n') {
+    correctAnswersCount++;
+    ageAnswer = true;
+} else {
+    alert('Sory, You should remeber my age, I am 33');
+}
+
+while (!genderValid) {
+    gender = prompt('My Gender is Male', 'Yes, Y, No or N').toLocaleLowerCase();
+    if (gender == 'yes' || gender == 'y' || gender == 'no' || gender == 'n') {
+        genderValid = true;
+    }
+}
+
+if (gender == 'yes' || gender == 'y') {
+    correctAnswersCount++;
+    genderAnswer = true;
+} else {
+    alert('Sory, You should remeber my gender, I am Male');
+}
+
+while (!favColorValid) {
+    favColor = prompt('My favourite color is Blue', 'Yes, Y, No or N').toLocaleLowerCase();
+    if (favColor == 'yes' || favColor == 'y' || favColor == 'no' || favColor == 'n') {
+        favColorValid = true;
+    }
+}
+
+if (favColor == 'no' || favColor == 'n') {
+    correctAnswersCount++;
+    favColorAnswer = true;
+} else {
+    alert('Sory, You should remeber my Fvourite color, It is Red');
+}
+
+while (!educationDegreeValid) {
+    educationDegree = prompt('My education grade is Master', 'Yes, Y, No or N').toLocaleLowerCase();
+    if (educationDegree == 'yes' || educationDegree == 'y' || educationDegree == 'no' || educationDegree == 'n') {
+        educationDegreeValid = true;
+    }
+}
+
+if (educationDegree == 'yes' || educationDegree == 'y') {
+    correctAnswersCount++;
+    educationDegreeAnswer = true;
+} else {
+    alert('Sory, You should remeber my Grade, I am a master degree');
+}
 
 var randomNumberAttempt = 0;
-function GuessGame() {
-    while (!randomNumberValid && randomNumberAttempt < 4) {
-        randomNumber = prompt('Chose number Between 100 and 200', 'for example: ' + myRandomNumber);
-        randomNumberAttempt++;
-        if (randomNumber > 200 || randomNumber < 100) {
-            alert('out of range');
+while (!randomNumberValid && randomNumberAttempt < 4) {
+    randomNumber = prompt('Chose number Between 100 and 200', 'for example: ' + myRandomNumber);
+    randomNumberAttempt++;
+    if (randomNumber > 200 || randomNumber < 100) {
+        alert('out of range');
 
-        } else {
-            if (myRandomNumber == randomNumber) {
-                randomNumberValid = true;
-                correctAnswersCount++;
-                randomNumberAnswer = true;
-
-
-            }
-            else if (randomNumber > myRandomNumber + 10) {
-                alert('you entered too high number');
-            } else if (randomNumber < myRandomNumber - 10) {
-                alert('you entered too low number');
-            }
+    } else {
+        if (myRandomNumber == randomNumber) {
+            randomNumberValid = true;
+            correctAnswersCount++;
+            randomNumberAnswer = true;
         }
-
-
-        if (randomNumberAttempt == 4) {
-            alert('You have exhausted four attempts, the right answer is: ' + myRandomNumber);
+        else if (randomNumber > myRandomNumber + 10) {
+            alert('you entered too high number');
+        } else if (randomNumber < myRandomNumber - 10) {
+            alert('you entered too low number');
         }
     }
-
+    if (randomNumberAttempt == 4) {
+        alert('You have exhausted four attempts, the right answer is: ' + myRandomNumber);
+    }
 }
-GuessGame();
+
+
 var belovedPlacesAttempt = 0;
+while (!belovedPlacesValid && belovedPlacesAttempt < 6) {
+    belovedPlaces = prompt('Guess one of my beloved places').toLocaleLowerCase();
 
-function GuessGametwo(){
-    
-    while (!belovedPlacesValid && belovedPlacesAttempt < 6) {
-        belovedPlaces = prompt('Guess one of my beloved places').toLocaleLowerCase();
-
-        for (var i = 0; i < myBelovedPlaces.length; i++) {
-            if (myBelovedPlaces[i] == belovedPlaces) {
-                //alert('You got it');
-                belovedPlacesValid = true;
-                correctAnswersCount++;
-                belovedPlacesAnswer = true;
-            }
-            //break;        
+    for (var i = 0; i < myBelovedPlaces.length; i++) {
+        if (myBelovedPlaces[i] == belovedPlaces) {
+            //alert('You got it');
+            belovedPlacesValid = true;
+            correctAnswersCount++;
+            belovedPlacesAnswer = true;
         }
-        if (belovedPlacesAnswer) {
-            alert('You got it');
-        } else {
-            alert('Wrong Answer');
-        }
-
-        // var wrongAnswersCounter=0;
-        // for (var j = 0; j < myBelovedPlaces.length; j++) {
-        //     if (myBelovedPlaces[i] != belovedPlaces) {
-        //         wrongAnswersCounter++;
-        //        }
-        //        if(wrongAnswersCounter==6){
-        //         alert('Wrong Answer');
-        //        }
-        // }
-
-        belovedPlacesAttempt++;
-        if (belovedPlacesAttempt == 6) {
-            alert('You have exhausted six attempts, the right answer is: ' + myBelovedPlaces);
-        }
-
     }
+    if (belovedPlacesAnswer) {
+        alert('You got it');
+    } else {
+        alert('Wrong Answer');
+    }
+    belovedPlacesAttempt++;
+    if (belovedPlacesAttempt == 6) {
+        alert('You have exhausted six attempts, the right answer is: ' + myBelovedPlaces);
+    }
+
 }
-GuessGametwo();
-// if (educationDegree == 'yes' || educationDegree == 'y') {
-//     correctAnswersCount++;
-//     educationDegreeAnswer = true;
-// } else {
-//     alert('Sory, You should remeber my Grade, I am a master degree');
-// }
 
+console.log('name: ',name);
+console.log('age: ',age);
+console.log('gender: ',gender);
+console.log('favColor: ',favColor);
+console.log('educationDegree: ',educationDegree);
+console.log('randomNumber: ',randomNumber);
+console.log('belovedPlaces: ',belovedPlaces);
 
-
-
+console.log('nameAnswer: ',nameAnswer);
+console.log('ageAnswer: ',ageAnswer);
+console.log('genderAnswer: ',genderAnswer);
+console.log('favColorAnswer: ',favColorAnswer);
+console.log('educationDegreeAnswer: ',educationDegreeAnswer);
+console.log('randomNumberAnswer: ',randomNumberAnswer);
+console.log('belovedPlacesAnswer: ',belovedPlacesAnswer);
 
 document.getElementById('myName').innerHTML = 'My Name is: ' + myName;
 document.getElementById('myAge').innerHTML = 'My Age is: ' + myAge;
 document.getElementById('myGender').innerHTML = 'My Gender is: ' + myGender;
 document.getElementById('myEducationGrade').innerHTML = 'My Grade is: ' + myEducationDegree;
 document.getElementById('myFavColor').innerHTML = 'My Favourite color is: ' + myFavColor;
+document.getElementById('myRandomNumber').innerHTML = 'The Random Number is: ' + myRandomNumber;
+document.getElementById('myBelovedPlaces').innerHTML = 'My Beloved places are: ' + myBelovedPlaces;
 
 document.getElementById('correctAnswers').innerHTML = 'Number of correct answers:' + correctAnswersCount;
 
@@ -184,6 +204,20 @@ if (educationDegree == 'yes' || educationDegree == 'y') {
     myEducationGrade
 }
 
+if (randomNumberAnswer == true) {
+    document.getElementById('myRandomNumber').style.color = 'green';
+    myEducationGrade
+}
+
+if (belovedPlacesAnswer == true) {
+    document.getElementById('myBelovedPlaces').style.color = 'green';
+    myEducationGrade
+}
+
+
+
+
+
 // console.log('User answer for name: ' + nameAnswer);
 // console.log('User answer for age: ' + ageAnswer);
 // console.log('User answer for education grade : ' + genderAnswer);
@@ -191,3 +225,9 @@ if (educationDegree == 'yes' || educationDegree == 'y') {
 // console.log('User answer for gender: ' + educationDegreeAnswer);
 
 alert('Wellcome ' + userName + ', You answer ' + correctAnswersCount + ' correctly out of 7');
+
+
+
+
+
+
